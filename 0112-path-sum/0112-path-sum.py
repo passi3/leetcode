ae101358f7ee6b193b22a057092e3864865e7fdb
passi <138092466+passi3@users.noreply.github.com>
2024-08-14 10:13:39 +0900

    Time: 50 ms (6.35%), Space: 17.5 MB (37.88%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -9,7 +9,7 @@ class Solution:
         if root is None:
             return False    # 빈 노드는 False 반환
         
-        if not root.left and not root.right and root.val == targetSum:
+        if root.left == None and root.right == None and root.val == targetSum:
             return True # 현재 노드가 leaf이고 값이 targetSum과 같은 경우 True 반환
 
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)    # recursive로 root의 왼쪽 또는 오른쪽 노드 호출하며, 그때의 target 값을 현재 노드의 값을 뺌으로써 업데이트
