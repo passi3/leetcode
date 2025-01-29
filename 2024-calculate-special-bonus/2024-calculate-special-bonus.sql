5b7fad639f8841a1240e6848513ab905b3f0048e
nowj8n <138092466+passi3@users.noreply.github.com>
2025-01-29 12:09:54 +0900

    Time: 1291 ms (22.76%), Space: 0B (100%) - LeetHub

---
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT employee_id, CASE WHEN MOD(employee_id, 2) = 1 AND LEFT(name, 1) != 'M' THEN salary ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id;
