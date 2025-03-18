28b627eb051a902c52185ae2c61515185aeaa932
nowj8n <138092466+passi3@users.noreply.github.com>
2025-03-18 15:44:59 +0900

    Time: 815 ms (28.42%), Space: 0B (100%) - LeetHub

---
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+WITH DailyAmount AS (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+),
+WithWindow AS (
+    SELECT d1.visited_on, SUM(d2.amount) AS amount,
+    ROUND(AVG(d2.amount), 2) AS average_amount
+    FROM DailyAmount d1 JOIN DailyAmount d2
+    ON d2.visited_on BETWEEN DATE_SUB(d1.visited_on, INTERVAL 6 DAY) AND d1.visited_on
+    GROUP BY d1.visited_on
+)
+
+SELECT *
+FROM WithWindow
+WHERE visited_on >= (
+    SELECT MIN(visited_on) + INTERVAL 6 DAY
+    FROM DailyAmount
+)
+ORDER BY visited_on;
