34ff4d857858511d560a221645d17f14b4f9c0f2
nowj8n <138092466+passi3@users.noreply.github.com>
2025-03-27 21:46:58 +0900

    Time: 281 ms (93.32%), Space: 67.8 MB (5.24%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values('id', inplace=True)
-    person.drop(person[person.duplicated(subset='email', keep='first')].index, inplace=True)
+    person.drop_duplicates("email", inplace=True)
