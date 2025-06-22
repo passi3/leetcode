abc0aa60483ac59e76ae1ce8061edd93eece2b30
nowj8n <138092466+passi3@users.noreply.github.com>
2025-06-22 12:37:28 +0900

    Time: 307 ms (87.15%), Space: 68.3 MB (72.2%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.py
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
 def price_at_given_date(products: pd.DataFrame) -> pd.DataFrame:
-    product_ids = products["product_id"].unique()
-    products = products.sort_values("change_date", ascending=False)
-    products = products[products["change_date"] <= "2019-08-16"].drop_duplicates("product_id")
-    result = pd.DataFrame({
-        "product_id": product_ids,
-    })
-    result = result.merge(products[["product_id", "new_price"]], on="product_id", how="left").rename(columns={"new_price": "price"}).fillna(10)
+    filtered = (
+        products[products["change_date"] <= "2019-08-16"]
+        .sort_values("change_date", ascending=False)
+        .drop_duplicates("product_id")
+    )
+
+    result = (
+        pd.DataFrame({"product_id": products["product_id"].unique()})
+        .merge(filtered[["product_id", "new_price"]], on="product_id", how="left")
+        .rename(columns={"new_price": "price"})
+        .fillna(10)
+    )
+
     return result
