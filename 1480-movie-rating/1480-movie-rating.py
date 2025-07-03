da21d9e3f05175f3d8fc1aaf9dba1bef35b6eb41
nowj8n <138092466+passi3@users.noreply.github.com>
2025-07-03 13:54:18 +0900

    Time: 379 ms (85.86%), Space: 68.5 MB (85.6%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.py
+++ 1480-movie-rating/1480-movie-rating.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 def movie_rating(movies: pd.DataFrame, users: pd.DataFrame, movie_rating: pd.DataFrame) -> pd.DataFrame:
-    grouped_user_rating = movie_rating.groupby("user_id")["rating"].size().reset_index(name="reviews")
-    users = users.merge(grouped_user_rating, on="user_id", how="left")
-    movie_rating = movie_rating[(movie_rating["created_at"].dt.year==2020) & (movie_rating["created_at"].dt.month==2)]
-    grouped_movie_rating = movie_rating.groupby("movie_id")["rating"].mean().reset_index(name="avg_rating")
-    movies = movies.merge(grouped_movie_rating, on="movie_id", how="left")
-    most_rating_movie = movies.sort_values(["avg_rating", "title"], ascending=[False, True]).iloc[0]["title"]
-    most_reviewer = users.sort_values(["reviews", "name"], ascending=[False, True]).iloc[0]["name"]
-    return pd.DataFrame({
-        "results": [most_reviewer, most_rating_movie]
-    })
+    user = movie_rating.groupby("user_id").size().reset_index(name="count")
+    user = user.merge(users).sort_values(["count", "name"], ascending=[False, True])
+    user = user["name"].iloc[0]
+    
+    movie = movie_rating[(movie_rating["created_at"].dt.year==2020) & (movie_rating["created_at"].dt.month==2)]
+    movie = movie.groupby("movie_id")["rating"].mean().reset_index(name="rating")
+    movie = movie.merge(movies).sort_values(["rating", "title"], ascending=[False, True])
+    movie = movie["title"].iloc[0]
+    
+    return pd.DataFrame({"results": [user, movie]})
