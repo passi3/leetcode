949a07ac0f54a080e43436b6cfbef6966e727d08
nowj8n <138092466+passi3@users.noreply.github.com>
2025-07-09 13:15:29 +0900

    Time: 287 ms (14.84%), Space: 66.2 MB (76.56%) - LeetHub

---
+++ 3711-first-letter-capitalization-ii/3711-first-letter-capitalization-ii.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def capitalize_content(user_content: pd.DataFrame) -> pd.DataFrame:
+    user_content["converted_text"] = user_content["content_text"].str.title()
+    user_content.rename(columns={"content_text": "original_text"}, inplace=True)
+    return user_content
