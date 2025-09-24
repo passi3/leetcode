9a1c55c052ff9c2bf2928bbf3187764c68ecadde
JeJun Lee <138092466+passi3@users.noreply.github.com>
2025-09-24 16:57:15 +0900

    Time: 36 ms (70.33%), Space: 17.5 MB (99.16%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,14 +1,3 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        res = ""
-        prev = False
-        for letter in command:
-            if letter == "(":
-                prev = True
-            elif prev and letter == ")":
-                prev = False
-                res += "o"
-            elif letter not in [")"]:
-                prev = False
-                res += letter
-        return res
+        return command.replace("()", "o").replace("(al)", "al")
