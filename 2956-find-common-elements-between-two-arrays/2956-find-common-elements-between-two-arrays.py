1b4f5b48567087c236863bdc83cdd6da39904e27
JeJun Lee <138092466+passi3@users.noreply.github.com>
2025-11-30 10:22:04 +0900

    Time: 7 ms (71.8%), Space: 17.8 MB (81.81%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.py
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.py
@@ -3,12 +3,12 @@ class Solution:
 
         def cntIndicesInNums(nums1: List[int], nums2: List[int]) -> int:
             cnt = 0
+            nums2 = set(nums2)
             for num in nums1:
                 if num in nums2:
                     cnt += 1
             
             return cnt
 
-        print(cntIndicesInNums(nums2, nums1))
         return [cntIndicesInNums(nums1, nums2), cntIndicesInNums(nums2, nums1)]
         
