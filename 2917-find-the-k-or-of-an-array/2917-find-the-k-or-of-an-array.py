634ec93ca6838ea5af1693a26492c750d6c820d2
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-01-20 21:48:37 +0900

    Time: 23 ms (75.54%), Space: 19.4 MB (16.3%) - LeetHub

---
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findKOr(self, nums: List[int], k: int) -> int:
+        res = 0
+
+        for bit in range(32):
+            cnt = 0
+            for num in nums:
+                if (num >> bit) & 1:
+                    cnt += 1
+            
+            if cnt >= k:
+                res |= (1 << bit)
+        
+        return res
