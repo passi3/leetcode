9c1324a2b6f44f64be5330d96d1ef6820e14594e
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-02-16 15:05:10 +0900

    Time: 0 ms (100%), Space: 19.4 MB (50%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
@@ -1,21 +1,6 @@
 class Solution:
     def minChanges(self, n: int, k: int) -> int:
-        if n == k:
-            return 0
-        
-        res = 0
-        stack = []
-        binN = bin(n)[2:]
-        binK = bin(k)[2:]
-        print(binN, binK)
-        length = min(len(binN), len(binK))
-        if len(binN) > length:
-            res += binN[-length-1::-1].count("1")
-        elif len(binN) < len(binK):
+        if n & k != k:
             return -1
-        for i in range(-1, -length-1, -1):
-            if binN[i] == "1" and binN[i] != binK[i]:
-                res += 1
-            elif binN[i] == "0" and binN[i] != binK[i]:
-                return -1
-        return res
+        
+        return bin(n^k).count("1")
