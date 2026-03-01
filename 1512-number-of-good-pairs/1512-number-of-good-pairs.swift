7a7fb335494226c68bbbbc0e09bc55623d9de0e2
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-03-01 22:22:19 +0900

    Time: 0 ms (100%), Space: 19.5 MB (62%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func numIdenticalPairs(_ nums: [Int]) -> Int {
+        var res = 0
+        for i in 0..<nums.count {
+            for j in (i+1)..<nums.count {
+                if nums[i] == nums[j] {
+                    res += 1
+                }
+            }
+        }
+        return res
+    }
+}
