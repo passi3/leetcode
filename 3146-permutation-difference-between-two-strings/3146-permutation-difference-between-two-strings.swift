00e393fc2f6151c76f4a8488d187a0c645c2545f
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-03-02 16:03:42 +0900

    Time: 3 ms (60%), Space: 20.4 MB (20%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.swift
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.swift
@@ -1,9 +1,19 @@
 class Solution {
     func findPermutationDifference(_ s: String, _ t: String) -> Int {
         var res = 0
-        for (i, v) in s.enumerated() {
-            let idx = t.firstIndex(of: v)!
-            res += abs(i - t.distance(from: t.startIndex, to: idx))
+        var dict: Dictionary<Character, Int> = [:]
+        
+        for i in 0..<t.count {
+            let index = t.index(t.startIndex, offsetBy: i)
+            let value = t[index]
+            dict[value] = i
+        }
+
+        for i in 0..<s.count {
+            let index = s.index(s.startIndex, offsetBy: i)
+            let value = s[index]
+            let diff = abs(i-(dict[value] ?? 0))
+            res += diff
         }
         return res
     }
