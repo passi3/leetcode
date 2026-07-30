7a3e753273d9c66894d51002acaed28195d56e07
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-07-30 11:44:47 +0900

    Time: 0 ms (100%), Space: 19.4 MB (19.91%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -1,10 +1,3 @@
 class Solution:
     def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
-        def invertSec(str) -> str:
-            hh = int(str[:2])
-            mm = int(str[-2:])
-            return hh*(60**2) + mm*60
-        start = max(invertSec(event1[0]), invertSec(event2[0]))
-        end = min(invertSec(event1[1]), invertSec(event2[1]))
-        
-        return start <= end
+        return max(event1[0], event2[0]) <= min(event1[1], event2[1])
