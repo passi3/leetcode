6b6143d882dc192413cb013364b09c6e2938b9aa
JeJun Lee <138092466+passi3@users.noreply.github.com>
2026-08-17 15:47:43 +0900

    Time: 32 ms (95.84%), Space: 54.1 MB (11.22%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,10 +2,9 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if (this.length === 0) {
-        return -1;
-    }
-    return this[this.length-1];
+    if(!this.length) return -1
+    
+    return this.at(-1)
 };
 
 /**
